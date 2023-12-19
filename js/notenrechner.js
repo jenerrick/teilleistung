d68f73c86afd727f74ae1856d3687f59dc4938ad
jenerrick <118598887+jenerrick@users.noreply.github.com>
2023-12-19 14:50:38 +0100

    HTML-IDs hinzugefügt. berechneErgebnis-Funktion verallgemeinert. gib_aus-Funktion implementiert.
    Weitere Abstraktion der Programmteile geschaffen.

--- js/notenrechner.js
+++ js/notenrechner.js
@@ -1,21 +1,59 @@
-function berechneErgebnis() {
-    let felder = document.getElementsByName("gs")
+function berechneErgebnis(name) {
+    let felder = document.getElementsByName(name)
     let summe = 0
 
 
     felder.forEach(function (feld) {
         summe += parseInt(feld.value) || 0
+        summe / anzahleingabefelder
     })
-    
-    let ergebnis = document.getElementById("gs_ergebnis")
-    ergebnis.innerHTML = summe/4 + " NP"
+
+    gib_aus(ausgabefeld, summe)
 }
+function gib_aus(ausgabefeld, summe) {
+    let ergebnis = document.getElementById(ausgabefeld)
+    ergebnis.innerHTML = summe + " NP"
 
-
+}
 
 document.getElementById("gs").addEventListener(
     "input",
     function () {
-        berechneErgebnis();
+        berechneErgebnis("gs");
+    }
+)
+
+document.getElementById("h1").addEventListener(
+    "input",
+    function () {
+        berechneErgebnis("h1");
+    }
+)
+
+document.getElementById("h2").addEventListener(
+    "input",
+    function () {
+        berechneErgebnis("h2");
+    }
+)
+
+document.getElementById("h3").addEventListener(
+    "input",
+    function () {
+        berechneErgebnis("h3");
+    }
+)
+
+document.getElementById("da").addEventListener(
+    "input",
+    function () {
+        berechneErgebnis("da");
+    }
+)
+
+document.getElementById("praktika").addEventListener(
+    "input",
+    function () {
+        berechneErgebnis("praktika");
     }
 )
