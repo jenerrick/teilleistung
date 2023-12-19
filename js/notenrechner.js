ea50cccb55df76e581f38a002e80359fda99d380
jenerrick <118598887+jenerrick@users.noreply.github.com>
2023-12-19 15:03:03 +0100

    Durchschnitt in Rechenfunktion hinzugefügt Keine Darstellung der Summe als Durchschnitt mehr

--- js/notenrechner.js
+++ js/notenrechner.js
@@ -1,60 +1,63 @@
-function berechneErgebnis(name) {
+function berechneErgebnis(name,ausgabefeld) {
     let felder = document.getElementsByName(name)
     let summe = 0
 
     felder.forEach(function (feld) {
         summe += parseInt(feld.value) || 0
-        summe / anzahleingabefelder
     })
 
-    gib_aus(ausgabefeld, summe)
+    let durchschnitt = 0
+    if (felder.length > 0) {
+        durchschnitt = summe / felder.length
+    }
+    gib_aus(ausgabefeld, durchschnitt)
 }
 
-function gib_aus(ausgabefeld, summe) {
+function gib_aus(ausgabefeld, durchschnitt) {
     let ergebnis = document.getElementById(ausgabefeld)
-    ergebnis.innerHTML = summe + " NP"
+    ergebnis.innerHTML = durchschnitt + " NP"
 
 }
 
 document.getElementById("gs").addEventListener(
     "input",
     function () {
-        berechneErgebnis("gs","gs_ergebnis");
+        berechneErgebnis("gs", "gs_ergebnis");
     }
 )
 
 document.getElementById("h1").addEventListener(
     "input",
     function () {
-        berechneErgebnis("h1","h1_ergebnis");
+        berechneErgebnis("h1", "h1_ergebnis");
     }
 )
 
 document.getElementById("h2").addEventListener(
     "input",
     function () {
-        berechneErgebnis("h2","h2_ergebnis");
+        berechneErgebnis("h2", "h2_ergebnis");
     }
 )
 
 document.getElementById("h3").addEventListener(
     "input",
     function () {
-        berechneErgebnis("h3","h3_ergebnis");
+        berechneErgebnis("h3", "h3_ergebnis");
     }
 )
 
 document.getElementById("da").addEventListener(
     "input",
     function () {
-        berechneErgebnis("da","da_ergebnis");
+        berechneErgebnis("da", "da_ergebnis");
     }
 )
 
 document.getElementById("praktika").addEventListener(
     "input",
     function () {
-        berechneErgebnis("praktika","praktika_ergebnis")
+        berechneErgebnis("praktika", "praktika_ergebnis")
     }
 )
 
