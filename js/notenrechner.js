76bfeec0392c3536fe80104eb7c98cc17229f400
jenerrick <118598887+jenerrick@users.noreply.github.com>
2024-01-12 18:19:59 +0100

    Änderung von ids zu Klassen in Gesamtnotentabelle
    Die IDs von den td-Elementen für die Gesamtnote und die Gesamtnote als
    Dezimalnote wurden zu Klassen verändert, damit die Funktion gib_aus
    ordnungsgemäß funktioniert. Die Variablen für die einzelnen Hauptstudienabschnitte
    wurden komplementär zur HTML-Struktur zu einer Variable für das gesamte Hauptstudium
    zusammengefasst.

--- js/notenrechner.js
+++ js/notenrechner.js
@@ -1,9 +1,7 @@
 // Hier werden die Variablen für die Durchschnittsergebnisse der einzelnen Bestandteile des Studiums deklariert. 
 // Diese werden später in der Funktion berechneDurchschnittsnote verwendet.
 let zwischenergebnisGS = 0
-let zwischenergebnisH1 = 0
-let zwischenergebnisH2 = 0
-let zwischenergebnisH3 = 0
+let zwischenergebnisHS = 0
 let zwischenergebnisDA = 0
 let zwischenergebnisPraktika = 0
 
@@ -29,15 +27,15 @@ function berechneZwischenergebnis(name) {
 
     if (name == "gs") {
         zwischenergebnisGS = durchschnitt
-        gib_aus("gs_ergebnis",durchschnitt)
+        gib_aus("gs_ergebnis", durchschnitt)
     }
     if (name === "hs") {
         zwischenergebnisHS = durchschnitt
-        gib_aus("hs_ergebnis",durchschnitt)
+        gib_aus("hs_ergebnis", durchschnitt)
     }
     if (name == "praktika") {
         zwischenergebnisPraktika = durchschnitt
-        gib_aus("praktika_ergebnis",durchschnitt)
+        gib_aus("praktika_ergebnis", durchschnitt)
     }
 }
 
@@ -58,17 +56,96 @@ function berechneZwischenergebnisDA() {
 
 /**
  * Diese Funktion berechnet die Durchschnittsnote für das gesamte Studium. Dazu werden die einzelnen zuvor errechneten Zwischenergebnisse gewichtet addiert 
- * und über die Funktion gib_ausaufgerufen. Die Erfordernis eines Parameters, der gib_auf übergeben werden kann, entfällt, weil die Durchschnittsnote immer
- * im Element mit der Klasse "gesamt_ergebnis" ausgegeben wird.
+ * und über die Funktion gib_aus ausgegeben. 
  * 
  */
-function berechneDurchschnittsnote(ausgabefeld) {
+function berechneDurchschnittsnote() {
     let durchschnitt = 0
+    let durchschnittDezimal = 0
+
     durchschnitt += zwischenergebnisGS * 0.05
-    durchschnitt += (zwischenergebnisH1 + zwischenergebnisH2 + zwischenergebnisH3) / 3 * 0.7
+    durchschnitt += zwischenergebnisHS * 0.7
     durchschnitt += zwischenergebnisDA * 0.2
     durchschnitt += zwischenergebnisPraktika * 0.05
+
     gib_aus("gesamt_ergebnis", durchschnitt)
+    gib_aus("gesamt_ergebnis_dezimal",durchschnittDezimal)
+
+}
+
+/**
+ * Diese Funktion errechnet aus dem Notendurchschnitt den Durchschnitt als dezimale Note. Die Berechnung erfolgt anhand der Tabelle in der Anlage
+ * zu § 25 I S. 2 GVIDVDV
+ * @param {} durchschnitt Dieser Parameter ist für die Konvertierung in eine dezimale Note erforderlich.
+ * @returns 
+ */
+function berechneDurchschnittsnoteDezimal(durchschnitt) {
+    if (durchschnitt == 15) {
+        return 1.0
+    }   else if (durchschnitt >= 14,7) {
+        return 1,1
+    }   else if (durchschnitt >= 14,4) {
+        return 1,2
+    }   else if (durchschnitt >= 14,1) {
+        return 1,3
+    }   else if (durchschnitt >= 13,8) {
+        return 1,4
+    }   else if (durchschnitt >= 13,5) {
+        return 1,5
+    }   else if (durchschnitt >= 13,2) {
+        return 1,6
+    }   else if (durchschnitt >= 12,9) {
+        return 1,7
+    }   else if (durchschnitt >= 12,6) {
+        return 1,8
+    }   else if (durchschnitt >= 12,3) {
+        return 1,9
+    }   else if (durchschnitt >= 12,0) {
+        return 2,0
+    }   else if (durchschnitt >= 11,7) {
+        return 2,1
+    }   else if (durchschnitt >= 11,4) {
+        return 2,2
+    }   else if (durchschnitt >= 11,1) {
+        return 2,3
+    }   else if (durchschnitt >= 10,8) {
+        return 2,4
+    }   else if (durchschnitt >= 10,5) {
+        return 2,5
+    }   else if (durchschnitt >= 10,2) {
+        return 2,6
+    }   else if (durchschnitt >= 9,9) {
+        return 2,7
+    }   else if (durchschnitt >= 9,6) {
+        return 2,8
+    }   else if (durchschnitt >= 9,3) {
+        return 2,8
+    }   else if (durchschnitt >= 9,0) {
+        return 3,0
+    }   else if (durchschnitt >= 8,7) {
+        return 3,1
+    }   else if (durchschnitt >= 8,4) {
+        return 3,2
+    }   else if (durchschnitt >= 8,1) {
+        return 3,3
+    }   else if (durchschnitt >= 7,8) {
+        return 3,4
+    }   else if (durchschnitt >= 7,5) {
+        return 3,5
+    }   else if (durchschnitt >= 7,0) {
+        return 3,6
+    }   else if (durchschnitt >= 6,5) {
+        return 3,7
+    }   else if (durchschnitt >= 6,0) {
+        return 3,8
+    }   else if (durchschnitt >= 5,5) {
+        return 3,9
+    }   else if (durchschnitt >= 5,0) {
+        return 4,0
+    }   else {
+        return 0
+    }
+        
 
 }
 
@@ -80,7 +157,7 @@ function berechneDurchschnittsnote(ausgabefeld) {
 function gib_aus(ausgabefeld, durchschnitt) {
     let ergebnis = document.getElementsByClassName(ausgabefeld)
     for (let element of ergebnis) {
-        element.innerHTML = durchschnitt + " NP";
+        element.innerHTML = durchschnitt
     }
 
 }
