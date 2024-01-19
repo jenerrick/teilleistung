4d192e539d7fa3454474aeecfcc894446ce02b38
jenerrick <118598887+jenerrick@users.noreply.github.com>
2024-01-19 15:08:34 +0100

    Togglen der Klasse in neue Funktion ausgelagert. Die Klassen werden jetzt durch die Funktion klassenUmschalten umgeschaltet. Übersichtlichkeit und Wartbarkeit des Codes verbessert.

--- js/darkmode.js
+++ js/darkmode.js
@@ -10,12 +10,12 @@ function darkmodeUmschalten() {
     let main_container = document.querySelector("#main-container")
     let ausgabe_und_hinweis_container = document.querySelector("#div_ausgabe_und_hinweis")
 
-    //Links und Tabellen 
+    //Mehrfach auftauchende Elemente
     let navbar_links = document.getElementsByClassName("nav-link")
     let footer_links = document.getElementsByClassName("footer-links")
     let main_container_links = document.querySelectorAll("#main-container a")
     let tabellen = document.querySelectorAll("table")
-  
+
     //Button auf aktiv/inaktiv setzen
     if (ist_gedrückt) {
         button_darkmode.setAttribute("aria-pressed", "false");
@@ -25,47 +25,29 @@ function darkmodeUmschalten() {
         button_darkmode.classList.add("active");
     }
 
-    //Große Elemente: Klassen für Dark-/White-Mode togglen
-    navbar.classList.toggle("navbar-dark")
-    navbar.classList.toggle("bg-dark")
-    navbar.classList.toggle("navbar-light")
-    navbar.classList.toggle("bg-lime")
-
-    body.classList.toggle("bg-secondary")
-
-    footer.classList.toggle("bg-dark")
-    footer.classList.toggle("bg-lime")
-
-    main_container.classList.toggle("bg-dark")
-    main_container.classList.toggle("text-dark")
-    main_container.classList.toggle("bg-light")
-    main_container.classList.toggle("text-light")
-
- 
-    ausgabe_und_hinweis_container.classList.toggle("bg-secondary")
-    ausgabe_und_hinweis_container.classList.toggle("text-light")
-    ausgabe_und_hinweis_container.classList.toggle("bg-white")
-    ausgabe_und_hinweis_container.classList.toggle("text-dark")
-    
-
-
-    //Links: Klassen für Dark-/White-Mode togglen 
-    for (const link of navbar_links) {
-        link.classList.toggle("text-light")
-        link.classList.toggle("text-dark")
+    //Es gibt nicht auf jeder Seite eine Ausgabe- und Hinweis-Div
+    if (ausgabe_und_hinweis_container) {
+        klassenUmschalten([ausgabe_und_hinweis_container],["bg-secondary","bg-white","text-white","text-dark"])
     }
-    for (const footer_link of footer_links) {
-        footer_link.classList.toggle("text-light")
-        footer_link.classList.toggle("text-dark")
-    }
-    for (const main_container_link of main_container_links) {
-        main_container_link.classList.toggle("a-dark")
-    }
-    for (const tabelle of tabellen) {
-        tabelle.classList.toggle("table-dark")
-    }
-   
 
+    //Klassen einmaliger Elemente umschalten
+    klassenUmschalten([navbar,footer],["bg-dark","bg-lime"])
+    klassenUmschalten([navbar],["navbar-dark","navbar-light"])
+    klassenUmschalten([body],["bg-secondary"])
+    klassenUmschalten([main_container],["bg-dark","bg-light","text-light","text-dark"])
+
+    //Klassen von Mengen von Elementen umschalten
+    klassenUmschalten(footer_links,["text-light","text-dark"])
+    klassenUmschalten(navbar_links,["text-light","text-dark"])
+    klassenUmschalten(main_container_links,["a-dark"])
+    klassenUmschalten(tabellen,["table-dark"])
+}
+function klassenUmschalten(elemente, klassen) {
+    for (const element of elemente) {
+        for (const klasse of klassen) {
+            element.classList.toggle(klasse);
+        }
+    }
 }
 
 function init() {
